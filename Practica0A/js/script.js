f42279858da2e5e1e4476511baa443cd0e27d6cf
AleEsc0bar09 <alexandra.escobar2027@frech.superate.org>
2026-07-16 10:45:51 -0600

    Clase 1 de JS

---
+++ Practica0A/js/script.js
@@ -0,0 +1,10 @@
+
+
+function revisar (){
+    
+    let caja = document.getElementById("mensaje");
+    //obtenemos el parrafo donde ira el mensaje
+
+    caja.innerHTML = "Respuesta correcta!"
+    
+}
